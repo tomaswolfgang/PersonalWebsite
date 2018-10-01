578935be97583790ef3ccf810e8543e14440d3c6
Thomas Wong <tomaswolfgang@gmail.com>
2018-09-30 22:21:52 -0400

    added compression

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser= require('body-parser');
+const compression = require('compression');
 const app = express();
 const port = process.env.PORT || 3001;
 const path = require('path');
@@ -10,6 +11,7 @@ var db;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(staticFiles);
+app.use(compression());
 
 
 
